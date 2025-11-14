e55153ada1f3b43704798523c5a5d128b53401ec
Akooni <127095348+Akooni@users.noreply.github.com>
2025-11-15 00:14:32 +0400

    made layout for the even odd page add functionality

---
+++ even_odd.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Even or Odd</title>
+</head>
+<body>
+    <form method="get" action="even_odd.php">
+        <label for="number">Put a number it will tell you Even or Odd:</label><br>
+        <input type="number" name="num">
+        <input type="submit" value="Submit">
+
+
+    </form>
+</body>
+</html>
