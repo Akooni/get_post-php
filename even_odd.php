666218cf73f6f6e1289ff980cfcf8277f43f60c5
Akooni <127095348+Akooni@users.noreply.github.com>
2025-11-15 00:33:30 +0400

    fix bug where the number size and text size are showing different size

--- even_odd.php
+++ even_odd.php
@@ -6,12 +6,28 @@
     <title>Even or Odd</title>
 </head>
 <body>
-    <form method="get" action="even_odd.php">
+    <form method="post" action="even_odd.php">
         <label for="number">Put a number it will tell you Even or Odd:</label><br>
         <input type="number" name="num">
         <input type="submit" value="Submit">
 
 
     </form>
+
+    <?php
+    $space = " ";
+    if(isset($_POST['num'])) {
+        $num = (int)$_POST['num'];
+        
+        if($num % 2 === 0 ) {
+            echo '<h1>'.$num.$space.'is even</h1>';
+        } else {
+            echo '<h1>'.$num.$space.'is odd</h1>';
+        }
+    }
+    
+    
+    
+    ?>
 </body>
 </html>
